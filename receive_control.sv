4b795d68180610454932d1ae2e4afb24f4e0b702
TheMallornTimpanist <alex.b.valois@gmail.com>
2018-09-15 15:43:47 -0500

    Add files via upload

--- receive_control.sv
+++ receive_control.sv
@@ -1,51 +1,51 @@
-module transmit_control(TX, dataIn, TBR, baud, write, clk, rst_n);
+module recieve_control(RX, dataOut, RDA, baud, read, clk, rst_n);
 
-// RS232 UART transmitter for ECE 554 miniproject 1
+// RS232 UART reciever for ECE 554 miniproject 1
 
 ////////////////////////////////////////////////////////////////////////////////
 
 // I/O
-input[7:0] dataIn;
-input baud, write, clk, rst_n;
-output TX, TBR;
+output[7:0] dataOut;
+output RDA;
+input RX, baud, read, clk, rst_n;
 
 // Internal Sigs
 wire[7:0] nextWord;
 wire empty, full;
-reg[9:0] shiftReg;
+reg[8:0] shiftReg;
 reg [3:0] timer;
-reg readBuf, setTimer, tick, loadShift, shift;
+reg writeBuf, setTimer, tick, loadShift, shift;
 
 // State Type
-typedef enum reg[1:0] {IDLE, LOAD, TRANS} state_t;
+typedef enum reg[1:0] {IDLE, REC} state_t;
 state_t state, next_state;
 
 ////////////////////////////////////////////////////////////////////////////////
 
 // Shift Reg
 always @(posedge clk, negedge rst_n) begin
-	if (!rst_n) shiftReg <= 10'hfff;
-	else if (loadShift) shiftReg <= {1'b1, nextWord, 1'b0};
-	else if (shift) shiftReg <= {1'b1, shiftReg[9:1]};
+	if (!rst_n) shiftReg <= 9'h000;
+	else if (loadShift) shiftReg <= 9'h000;
+	else if (shift) shiftReg <= {RX, shiftReg[8:1]};
 	else shiftReg <= shiftReg;
 end
 
 // Timer
 always @(posedge clk, negedge rst_n) begin
 	if (!rst_n) timer <= 4'h0;
-	else if (setTimer) timer <= 4'ha;
+	else if (setTimer) timer <= 4'h9;
 	else if (tick) timer <= timer - 1'b1;
 	else timer <= timer;
 end
 
 // FIFO
-fifo buffer(dataIn, nextWord, empty, full, readBuf, write, clk, rst_n);
+fifo buffer(nextWord, dataOut, empty, full, read, writeBuf, clk, rst_n);
 
-// TBR
-assign TBR = !full;
+// RDA
+assign RDA = !empty;
 
-// TX
-assign TX = shiftReg[0];
+// nextWord
+assign nextWord = shiftReg[7:0];
 
 // State Flops
 always @(posedge clk, negedge rst_n) begin
@@ -56,7 +56,7 @@ end
 // State Logic
 always_comb begin
 	// Default Signals
-	readBuf = 1'b0;
+	writeBuf = 1'b0;
 	setTimer = 1'b0;
 	loadShift = 1'b0;
 	tick = 1'b0;
@@ -66,31 +66,28 @@ always_comb begin
 	case (state)
 		IDLE: begin
 			if (baud) begin
-				if (!empty) begin
+				if (!RX) begin
 					loadShift = 1'b1;
-					next_state = LOAD;
+					setTimer = 1'b1;
+					next_state = REC;
 				end
 				else next_state = IDLE;
 			end
 			else next_state = IDLE;
 		end
-		LOAD: begin
-			readBuf = 1'b1;
-			setTimer = 1'b1;
-			next_state = TRANS;
-		end
-		TRANS: begin
+		REC: begin
 			if (baud) begin
 				if (timer == 4'h0) begin
 					next_state = IDLE;
+					writeBuf = 1'b1;
 				end
 				else begin
 					tick = 1'b1;
 					shift = 1'b1;
-					next_state = TRANS;
+					next_state = REC;
 				end
 			end
-			else next_state = TRANS;
+			else next_state = REC;
 		end
 		default: next_state = IDLE;
 	endcase
